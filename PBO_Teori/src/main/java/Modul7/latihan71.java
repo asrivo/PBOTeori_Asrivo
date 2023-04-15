3548b6fa9cdab221d7781cb59f1bbf5a22123cc7
Asrivo Maha Kurnia Pitama <127371793+asrivo@users.noreply.github.com>
2023-04-15 16:38:59 +0700

    upload ulang teori

---
+++ PBO_Teori/src/main/java/Modul7/latihan71.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modul7;
+
+/**
+ *
+ * @author Asrivo MKP
+ */
+public class latihan71 {
+     public static void main(String[] args){
+        
+        String days[] = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+        
+         int i = 0;
+         do
+         {
+             System.out.println(days[i]);
+             i++;
+             
+         }while(i<days.length);
+    
+}
+}
